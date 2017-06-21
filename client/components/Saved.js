313c61587533f60041f29d90fa3dffd8110a5d01
Zach Gilseth <zach@Zachs-Mac-mini.local>
2017-06-21 16:30:03 -0400

    updated search box styles

--- client/components/Saved.js
+++ client/components/Saved.js
@@ -10,7 +10,7 @@ const Saved = React.createClass({
 		return (
 		  <div className="saved-keywords clearfix">
 
-		  	<h3>Saved Search Terms</h3>
+		  	<h3>My Saved Keywords</h3>
 
 			{ results.map((result, i) => <div className="saved-keyword" key={i} ><span className="keyword">{result}</span><span onClick={ e => this.props.remove_keyword( i ) } className="remove-btn">Remove</span></div> ) }
 
