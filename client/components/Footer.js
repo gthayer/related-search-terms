ee868d699ec421670c90d317b30dc10e372d3ddf
Gary Thayer <garythayer@Garys-MacBook-Pro.local>
2017-08-03 20:26:48 -0400

    footer styles

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -10,7 +10,7 @@ const Footer = React.createClass({
 				<div className="wrap">
 
 					<a target="_blank" title="Hall Internet Marketing" href="https://hallme.com"><img src="/assets/hall-logo-no-bg.png" alt="Hall Internet Marketing" /></a>
-					Provided by <a target="_blank" title="Hall Internet Marketing" href="https://hallme.com">Hall Internet Marketing</a>
+					<p>Provided by <a target="_blank" title="Hall Internet Marketing" href="https://hallme.com">Hall Internet Marketing</a></p>
 
 				</div>
 			</div>
